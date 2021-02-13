5dacbe400c5a43d21075528f6af3e433bb937d14
Tammarra McPhaul <shaemc22@gmail.com>
2021-02-13 17:48:52 -0500

    commit tree 50082b43c58abe2078b17f2a2ee9fc834b4db7df

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,32 @@
 class Triangle
-  # write code here
+
+  attr_accessor :length_1, :length_2, :length_3
+  
+  def initialize(length_1, length_2, length_3)
+    @length_1 = length_1
+    @length_2 = length_2
+    @length_3 = length_3
+  end
+
+  def kind
+    if length_1 > 0 && length_2 > 0 && length_3 > 0 && length_1 + length_2 > length_3 && length_2 + length_3 > length_1 && length_1 + length_3 > length_2
+      if length_1 == length_2 && length_2 == length_3
+        return :equilateral
+      elsif length_1 == length_2 || length_1 == length_3 || length_2 == length_3
+        return :isosceles
+      else 
+        return :scalene
+      end
+    else
+      raise TriangleError
+    end
+  end
+    
+  
+
+class TriangleError < StandardError
+   def message 
+   "This is an error"
+   end
+  end
 end
